ef445f31cf4ccff5499113d23769b8016c2de581
Jackie O'Donnell <jackieodonnell@Jackies-MBP.attlocal.net>
2022-11-30 22:35:52 -0500

    dashboard and activity pages setup, route api calls to updated server endpoints

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -3,12 +3,10 @@ import axios from "axios";
 export default {
 
     async getData(){
-        return await axios.get("http://localhost:3002/api/v1/data/admin");
-        // .then((res) => {
-
-        // }).catch((err) => {
-        //     console.log(err)
-        // });
+        return await axios.get("http://localhost:3002/api/v1/data/admin", { 
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+            }
+    });
     }
-
 }
